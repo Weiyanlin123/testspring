f20f6847521fda6729f34833a70dc3f0ffd779ec
Weiyanlin123 <17621257791@163.com>
2020-07-29 15:18:38 +0800

    dev最后的提交

--- src/main/java/com/citycloud/dcm/Application.java
+++ src/main/java/com/citycloud/dcm/Application.java
@@ -8,10 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.citycloud.dcm.street.mapper")
 public class Application {
     //dev分支
-
-    //测试合并
-
-    //合并远程分支
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
