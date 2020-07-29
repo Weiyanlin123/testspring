befc2d62239e7c69bd05137afcf1409ae1b2cc02
Weiyanlin123 <17621257791@163.com>
2020-07-29 15:49:29 +0800

    dev  double

--- src/main/java/com/citycloud/dcm/Application.java
+++ src/main/java/com/citycloud/dcm/Application.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.citycloud.dcm.street.mapper")
 public class Application {
+
     public static void main(String[] args) {
         Integer integer = new Integer(0);
+        Double aDouble = new Double(0);
         SpringApplication.run(Application.class, args);
     }
 
